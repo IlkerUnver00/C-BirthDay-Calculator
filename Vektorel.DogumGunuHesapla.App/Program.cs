eba0234b8da8c8c8cf542006489c9794c4dffcdd
İlker Ünver <108666062+IlkerUnver00@users.noreply.github.com>
2022-07-05 13:12:19 +0300

    Add files via upload

---
+++ Vektorel.DogumGunuHesapla.App/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vektorel.DogumGunuHesapla.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Yıl:");
+            //int dogumYili = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Doğum tarihinizi giriniz[gg.aa.yyyy]:");
+            //string dogumTarihi = Console.ReadLine();
+            //DateTime dogumT = Convert.ToDateTime(dogumTarihi);
+
+            //Console.WriteLine(dogumT.Year);
+            //Console.WriteLine(dogumT.Month);
+            //Console.WriteLine(dogumT.Day);
+            //Console.WriteLine(dogumT.Hour);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
